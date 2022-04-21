a1ac390a08c26b06c9d40ef11b4889173275b846
manthanghasadiya <201951065@iiitvadodara.ac.in>
2022-04-21 10:05:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './My components/Header';
+import {Todos} from './My components/Todos';
+import {Footer} from './My components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header title="My Todos List" searchBar={false}/>
+            <Todos/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
